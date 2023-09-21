b7fce3824c3786336ea499d4bcf740d9c7cc8ab1
Matheus Fagundes <124789617+m4Fagundes@users.noreply.github.com>
2023-09-21 11:30:49 -0300

    pasta front end e back end

--- Codigo/backend/src/main/java/com/EduJovem/backend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.EduJovem.backend.repository;
-
-import com.EduJovem.backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
