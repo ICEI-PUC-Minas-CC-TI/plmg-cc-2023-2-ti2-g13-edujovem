f8671f31182ab079b4305990c64ef81e7542b619
ThomNeuenschwander <130864240+ThomNeuenschwander@users.noreply.github.com>
2023-10-09 19:50:21 -0300

    BACKEND: authentication almost complete

---
+++ Codigo/backend/src/main/java/com/EduJovem/EduJovem.java
@@ -0,0 +1,38 @@
+package com.EduJovem;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.EduJovem.models.Role;
+import com.EduJovem.models.User;
+import com.EduJovem.repository.RoleRepository;
+import com.EduJovem.repository.UserRepository;
+
+@SpringBootApplication
+public class EduJovem {
+	public static void main(String[] args) {
+		SpringApplication.run(EduJovem.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncode){
+		return args ->{
+			if(roleRepository.findByAuthority("ADMIN").isPresent()) return;
+			Role adminRole = roleRepository.save(new Role("ADMIN"));
+			roleRepository.save(new Role("USER"));
+
+			Set<Role> roles = new HashSet<>();
+			roles.add(adminRole);
+
+			User admin = new User(1, "admin", passwordEncode.encode("password"), roles);
+
+			userRepository.save(admin);
+		};
+	}
+}
---
+++ Codigo/backend/src/main/java/com/EduJovem/controllers/AuthenticationController.java
@@ -0,0 +1,32 @@
+package com.EduJovem.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.EduJovem.models.LoginResponseDTO;
+import com.EduJovem.models.RegistrationDTO;
+import com.EduJovem.models.User;
+import com.EduJovem.services.AuthenticationService;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin("*")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody RegistrationDTO body){
+        return authenticationService.registerUser(body.getUsername(), body.getPassword());
+    }
+    
+    @PostMapping("/login")
+    public LoginResponseDTO loginUser(@RequestBody RegistrationDTO body){
+        return authenticationService.loginUser(body.getUsername(), body.getPassword());
+    }
+}   
