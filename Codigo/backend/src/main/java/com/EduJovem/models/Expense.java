c201b0bcd92bbd7819e2f2a6eef405897ca4823a
ThomNeuenschwander <130864240+ThomNeuenschwander@users.noreply.github.com>
2023-10-20 15:57:00 -0300

    BACKEND: Expense class

---
+++ Codigo/backend/src/main/java/com/EduJovem/models/Expense.java
@@ -0,0 +1,87 @@
+package com.EduJovem.models;
+
+import java.time.Instant;
+
+import com.EduJovem.models.enums.ExpenseCategory;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private Instant moment;
+    private String Description;
+    private ExpenseCategory category;
+    private Double value;
+
+
+    public Expense(){
+    }
+
+
+    public Expense(Integer id, Instant moment, String description, ExpenseCategory category, Double value) {
+        this.id = id;
+        this.moment = moment;
+        Description = description;
+        this.category = category;
+        this.value = value;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+
+    public String getDescription() {
+        return Description;
+    }
+
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+
+    public ExpenseCategory getCategory() {
+        return category;
+    }
+
+
+    public void setCategory(ExpenseCategory category) {
+        this.category = category;
+    }
+
+
+    public Double getValue() {
+        return value;
+    }
+
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+    
+    
+}
